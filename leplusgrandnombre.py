fed80fbb3ec8a6fc1f2e361d5273b59738cc7750
KODZO A. M. Mawuena <nashmichel70@gmail.com>
2021-10-08 13:47:52 +0000

    Ajout d'un nouveau script

---
+++ leplusgrandnombre.py
@@ -0,0 +1,19 @@
+#Netacademy by CISCO Exercies
+
+# Store the current largest number here.
+largest_number = -999999999
+
+# Input the first value.
+number = int(input("Enter a number or type -1 to stop: "))
+
+# If the number is not equal to -1, continue.
+while number != -1:
+    # Is number larger than largest_number?
+    if number > largest_number:
+        # Yes, update largest_number.
+        largest_number = number
+    # Input the next number.
+    number = int(input("Enter a number or type -1 to stop: "))
+
+# Print the largest number.
+print("The largest number is:", largest_number)
